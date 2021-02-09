952d7358f70ad8e65a4951915ceb12f00f346eda
gillerick <=>
2021-02-09 10:12:32 +0300

    Added port() function/Corrected router paths

--- Application/app.go
+++ Application/app.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"Users/Application/api"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main()  {
-http.HandleFunc("api/users/", RegistrationHandler)
-http.HandleFunc("api/user/", RegistrationHandler)
+http.HandleFunc("api/users/", api.RegistrationHandler)
+http.HandleFunc("api/user/", api.RegistrationHandler)
 
 http.ListenAndServe(port(), nil)
 log.Printf("Listening at port %s", port())
 
+}
+
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0{
+		port = "8080"
+	}
+	return ":"+port
 }
